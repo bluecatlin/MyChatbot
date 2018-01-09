3b79d9d0680100778e6a8d63fef21f9bf4807ff8
bluecatlin <bluecat.lin@hotmail.com>
2018-01-09 14:12:36 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -4,8 +4,9 @@
  //產生回傳給line server的格式
  $sender_userid = $json_obj->events[0]->source->userId;
  $sender_txt = $json_obj->events[0]->message->text;
+ $sender_replyToken = $json_obj->events[0]->replyToken;
  $response = array (
-				"to" => $sender_userid,
+				"reply" => $sender_replyToken,
 				"messages" => array (
 					array (
 						"type" => "text",
@@ -19,7 +20,7 @@
  //回傳給line server
  $header[] = "Content-Type: application/json";
  $header[] = "Authorization: Bearer ErrehF4NSPnq/XcPl8PSaAUczszYK2oQHWJt9FRrB+xVBC1fe8IdgNhBarExBhV1e80d4aQxytXLwEGuzx+Bb88MhInuckexJLRmNHTiv3e7MiXbHnWeu4HQEX3qNSYYbQiyoQND53tY+HSuHMnLPAdB04t89/1O/w1cDnyilFU=";
- $ch = curl_init("https://api.line.me/v2/bot/message/push");                                                                      
+ $ch = curl_init("https://api.line.me/v2/bot/message/reply");                                                                      
  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
