f478ca2337a505e5a84fd77aca5126f1e054e3e9
bluecatlin <bluecat.lin@hotmail.com>
2018-01-09 11:36:35 +0800

    get line message

--- receive.php
+++ receive.php
@@ -1,3 +1,8 @@
 <?php
-echo "hello! this is bluecat"
+  $json_str = file_get_contents('php://input');  // 接收REQUEST 的 BODY
+  $json_obj = json_decode($json_str);
+  
+  $myfile = fopen("log.txt", "w+") or die("Unable to open file!");
+  fwrite($myfile, "\xEF\xBB\xBF".$json_str); 
+  fclose($myfile);
 ?>
